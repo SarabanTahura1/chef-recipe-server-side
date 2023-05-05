7bd7b5b9c821042e29cd63ea1b867de53f903cb0
SarabanTahura1 <bristiakash01796@gmail.com>
2023-05-05 11:08:47 +0600

    cors install & json data add in project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
+app.use(cors());
+
+// default Route
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
