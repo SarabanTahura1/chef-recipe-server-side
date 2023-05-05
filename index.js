6fbbfe653140e818ac5467828dda2eda26353830
SarabanTahura1 <bristiakash01796@gmail.com>
2023-05-05 11:29:25 +0600

    dynamically route created for recipe

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+// import json data here
+const chefData = require("./data/data.json");
+const recipeData = require("./data/recepi.json");
 
 app.use(cors());
 
 // default Route
 app.get("/", (req, res) => {
   res.send("Server Running");
+});
+
+// route : chefData
+app.get("/chef-data", (req, res) => {
+  res.send(chefData);
+});
+
+// route : filter recipe by chefData
+app.get("/chef-data/:id", (req, res) => {
+  const id = req.params.id;
+  const FilterRecipe = recipeData?.filter((recipe) => recipe.category_id == id);
+  res.send(FilterRecipe);
 });
 
 app.listen(port, () => {
